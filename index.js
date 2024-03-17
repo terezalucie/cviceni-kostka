19e900475aa5ec35145b767e163f2764244af6cc
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-03-17 10:34:44 +0100

    01

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+let side = 1
+const img = document.querySelector("img")
+
+addEventListener("keyup", () => {
+    side++
+    if(side > 6){
+        side = 1
+    }
+    img.src = `img/side${side}.svg`
+})
